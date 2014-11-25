831b1fcad4aa2b50be188dfb4b411e197212631f
Shan Hasan <shan119@hotmail.co.uk>
2014-11-25 12:06:34 +0000

    updated Game function

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -6,8 +6,9 @@ Player.prototype.picks = function() {
 	// body...
 };
 
-function Game(){
-
+function Game(player1, player2){
+  this.player1 = player1;
+  this.player2 = player2;
 };
 
 Game.prototype.winner = function() {
