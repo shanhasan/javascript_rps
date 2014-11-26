882e41025226a40bcaec6971d842e1d0e2330d92
Shan Hasan <shan119@hotmail.co.uk>
2014-11-26 18:04:27 +0000

    created new array with previsouly selected element removed, to improve strategy

--- rpsls-javascript-jquery/lib/jquery_events.js
+++ rpsls-javascript-jquery/lib/jquery_events.js
@@ -3,33 +3,58 @@ $(document).ready(function(){
   var Computer = new Player ('Computer');
   var game = new Game (Shan, Computer);
   var history = new Array();
+  var arrayOfChoices = [];
+
+  $('.choices img').each(function() {
+    arrayOfChoices.push($(this).data('pick'));
+  });
 
   $('.choices img').on('click', function(){
-  	Shan.picks($(this).data('pick'));
+    Shan.picks($(this).data('pick'));
 
-		var arrayOfChoices = [];
-		$('.choices img').each(function() {
-			arrayOfChoices.push($(this).data('pick'));
-		});
-		Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
 
-    $('<li>' + game.winningMessage() + '</li>').prependTo('#results').fadeIn();
-    $('li:nth-child(5)').fadeOut(1000, function(){
-      $(this).remove();
-      })
-    $('#results').append('<li>' + game.winningMessage() + '</li>');
+      if (history.length == 0){
+        
+        Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
+      
+      } else {
 
-  	// $('li').each(function() {
-		// 	history.push($(this).text());
-		// });
-		
-		history.push(Shan.pick);
+        var computerChooses = [];
+        computerChooses = arrayOfChoices.slice();
 
-		if (history.length > 10){
-			history.shift();			
-		};
+        var lastSelection = history[history.length -1];
+        var lastSelectionIndex = computerChooses.indexOf(lastSelection);
 
-		console.log(history.join(" "));
+        console.log(computerChooses);
 
-	});
+      };
+      
+        $('<li>' + game.winningMessage() + '</li>').prependTo('#results').fadeIn();
+        $('li:nth-child(5)').fadeOut(1000, function(){
+          $(this).remove();
+          })
+        $('#results').append('<li>' + game.winningMessage() + '</li>');
+        
+        history.push(Shan.pick);
+
+        if (history.length > 10){
+          history.shift();      
+        };
+   });
 })
+
+    //console.log(history.join(" "));
+    // console.log(game.PAIRS);
+
+      // arrayOfChoices.splice(lastSelection);
+
+      // arrayOfChoices.remove(game.PAIRS[lastSelection][1]);
+
+      // arrayOfChoices.remove(game.PAIRS[lastSelection][3]);
+
+      // Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
+
+      // Computer.picks(lastSelection);
+
+      // Computer.picks(history.pop());
+      // console.log(history);
