e942ff58ada9db3d97c3cf75dc7a18966ecbab20
Ming Chan <ming.chan@windowslive.com>
2014-11-26 16:47:33 +0000

    results added to a 10 element history array

--- rpsls-javascript-jquery/lib/jquery_events.js
+++ rpsls-javascript-jquery/lib/jquery_events.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
-  var Ming = new Player ('Ming');
   var Shan = new Player ('Shan');
-  var game = new Game (Shan, Ming);
-  $('.choices img').on('click', function(){
-  	Ming.picks($(this).data('pick'));
+  var Computer = new Player ('Computer');
+  var game = new Game (Shan, Computer);
+  var history = new Array();
 
-		var array = [];
+  $('.choices img').on('click', function(){
+  	Shan.picks($(this).data('pick'));
+
+		var arrayOfChoices = [];
 		$('.choices img').each(function() {
-			array.push($(this).data('pick'));
+			arrayOfChoices.push($(this).data('pick'));
 		});
-		Shan.picks(array[Math.floor(Math.random() * array.length)]);
+		Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
 
     $('<li>' + game.winningMessage() + '</li>').prependTo('#results').fadeIn();
-    $('li:nth-child(5)').fadeOut(3000, function(){
+    $('li:nth-child(5)').fadeOut(1000, function(){
       $(this).remove();
       })
     $('#results').append('<li>' + game.winningMessage() + '</li>');
-  });
+
+  	// $('li').each(function() {
+		// 	history.push($(this).text());
+		// });
+		
+		history.push(Shan.pick);
+
+		if (history.length > 10){
+			history.shift();			
+		};
+
+		console.log(history.join(" "));
+
+	});
 })
