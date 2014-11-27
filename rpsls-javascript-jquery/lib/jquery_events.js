677889a28f8d4d8bcea802490c042079b72a777d
Shan Hasan <shan119@hotmail.co.uk>
2014-11-27 11:11:25 +0000

    fixed merge errors

--- rpsls-javascript-jquery/lib/jquery_events.js
+++ rpsls-javascript-jquery/lib/jquery_events.js
@@ -4,77 +4,6 @@ $(document).ready(function(){
   var game = new Game (Shan, Computer);
   var history = new Array();
   var arrayOfChoices = [];
-<<<<<<< HEAD
-
-  $('.choices img').each(function() {
-    arrayOfChoices.push($(this).data('pick'));
-  });
-
-  $('.choices img').on('click', function(){
-    Shan.picks($(this).data('pick'));
-
-
-      if (history.length == 0){
-        
-        Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
-      
-      } else {
-
-        var computerChooses = [];
-        computerChooses = arrayOfChoices.slice();
-
-        var lastSelection = history[history.length -1];
-        var lastSelectionIndex = computerChooses.indexOf(lastSelection);
-
-        computerChooses.splice(lastSelectionIndex, 1);
-
-        var rightObject = game.PAIRS
-
-        console.log(computerChooses);
-
-        console.log(game.PAIRS[lastSelection]);
-        
-        var justChecking = game.PAIRS[game.PAIRS.length -1];
-
-
-        console.log(this.justChecking);
-
-
-      };
-
-      
-        $('<li>' + game.winningMessage() + '</li>').prependTo('#results').fadeIn();
-        $('li:nth-child(5)').fadeOut(1000, function(){
-          $(this).remove();
-          })
-        $('#results').append('<li>' + game.winningMessage() + '</li>');
-        
-        history.push(Shan.pick);
-
-        if (history.length > 10){
-          history.shift();      
-        };
-
-
-
-    //console.log(history.join(" "));
-    // console.log(game.PAIRS);
-
-  });
-})
-      // arrayOfChoices.splice(lastSelection);
-
-      // arrayOfChoices.remove(game.PAIRS[lastSelection][1]);
-
-      // arrayOfChoices.remove(game.PAIRS[lastSelection][3]);
-
-      // Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
-
-      // Computer.picks(lastSelection);
-
-      // Computer.picks(history.pop());
-      // console.log(history);
-=======
   var computerChooses = [];
   var lastSelectionIndex;
 
@@ -146,4 +75,3 @@ $(document).ready(function(){
 	};
 
 });
->>>>>>> cc4c88d7ad4824d15c92e352aade239464def65f
