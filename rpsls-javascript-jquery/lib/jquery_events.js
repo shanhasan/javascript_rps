cc4c88d7ad4824d15c92e352aade239464def65f
Ming Chan <ming.chan@windowslive.com>
2014-11-27 00:59:57 +0000

    additional logic added

--- rpsls-javascript-jquery/lib/jquery_events.js
+++ rpsls-javascript-jquery/lib/jquery_events.js
@@ -3,16 +3,55 @@ $(document).ready(function(){
   var Computer = new Player ('Computer');
   var game = new Game (Shan, Computer);
   var history = new Array();
+  var arrayOfChoices = [];
+  var computerChooses = [];
+  var lastSelectionIndex;
+
+  $('.choices img').each(function() {
+  	arrayOfChoices.push($(this).data('pick'));
+  });
 
   $('.choices img').on('click', function(){
   	Shan.picks($(this).data('pick'));
 
-		var arrayOfChoices = [];
-		$('.choices img').each(function() {
-			arrayOfChoices.push($(this).data('pick'));
-		});
+	if (history.length == 0){
+
 		Computer.picks(arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)]);
 
+	} else {
+		computerChooses.length = 0;
+		computerChooses = arrayOfChoices.slice();
+		var lastSelection = history[history.length -1];
+		// lastSelectionIndex = computerChooses.indexOf(lastSelection);
+		// computerChooses.splice(lastSelectionIndex, 1);
+
+		removeItemFromArray(lastSelection);
+		removeItemFromArray(Object.keys(game.PAIRS[lastSelection])[0]);
+		removeItemFromArray(Object.keys(game.PAIRS[lastSelection])[1]);
+
+		Computer.picks(computerChooses[Math.floor(Math.random() * computerChooses.length)]);
+
+		console.log("Player 1 -> " + game.player1.pick + " & Computer -> " + game.player2.pick);
+		console.log(Object.keys(game.PAIRS[lastSelection])[0]);
+		console.log(Object.keys(game.PAIRS[lastSelection])[1]);
+		console.log("Computer then -> " + computerChooses);
+		console.log("++++++++++++++++++++++++++++++");
+
+
+
+
+		var frequency = {};
+
+		history.forEach(function(element){
+
+			frequency[element] === undefined ? frequency[element] = 1 : frequency[element]++;
+
+			console.log(frequency);
+		});
+
+	};
+
+		
     $('<li>' + game.winningMessage() + '</li>').prependTo('#results').fadeIn();
     $('li:nth-child(5)').fadeOut(1000, function(){
       $(this).remove();
@@ -28,8 +67,11 @@ $(document).ready(function(){
 		if (history.length > 10){
 			history.shift();			
 		};
-
-		console.log(history.join(" "));
-
 	});
-})
+
+  	removeItemFromArray = function(lastSelection){
+		lastSelectionIndex = computerChooses.indexOf(lastSelection);
+		computerChooses.splice(lastSelectionIndex, 1);
+	};
+
+});
